edd8d34e93f51e714cbf5d779ce12e2a70dd34d9
Rowell Sotto <github@sotto.org>
2016-09-19 23:04:40 -0700

    Use unmountComponentAtNode to get around RR warnings
    Trolling through source code, using unmountComponentAtNode appears to
    prevent the react router warnings. We can remove the hacky error filter.

--- src/client.js
+++ src/client.js
@@ -1,12 +1,11 @@
 /* eslint import/no-extraneous-dependencies: 0 */
 import React from 'react';
-import { render } from 'react-dom';
+import { render, unmountComponentAtNode } from 'react-dom';
 import { AppContainer } from 'react-hot-loader'; // Support hot loading
 
 // Import our react component that contains the react router
 import Root from './components/Root';
 // import Layout from './components/layout';
-import filterReactRouterHMRWarnings from './utils/filterReactRouterHMRWarnings';
 
 // By referencing our stylesheet entry point, this forces webpack to run it through
 // its loader system and makes it available as output.
@@ -20,18 +19,19 @@ const rootComponent = IS_PROD ?
                     <Root/> :
                     <AppContainer><Root/></AppContainer>;
 
+const DOM_MOUNT_POINT = document.querySelector('.appMountPoint');
 render(rootComponent,
         // render(<Layout/>,
-        document.querySelector('.appMountPoint'));
+        DOM_MOUNT_POINT);
 
 if (module.hot) { // This only runs in DEV
     module.hot.accept('./components/Root', () => {
         // Reload and rename the root module. You cannot reuse the Root
         // variable. The inexplicable will happen if you do.
         const HotRoot = require('./components/Root').default;   // eslint-disable-line
-        // Filter any react-router hot loader warnings
-        filterReactRouterHMRWarnings();
+        // Prevent the hot reloading error from react-router
+        unmountComponentAtNode(DOM_MOUNT_POINT);
         render(<AppContainer><HotRoot/></AppContainer>,
-            document.querySelector('.appMountPoint'));
+            DOM_MOUNT_POINT);
     });
 }
