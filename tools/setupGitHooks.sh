f6074edb52987b4124967b6d672e206fb4410027
Rowell Sotto <github@sotto.org>
2016-11-05 22:14:59 -0700

    Fixing githooks and linting script

--- tools/setupGitHooks.sh
+++ tools/setupGitHooks.sh
@@ -2,6 +2,8 @@
 
 # Sets up git hooks to prevent bad checkins from being uploaded to the origin
 echo "Symlinking hooks..."
-BASEDIR=$(cd $(dirname "$1") && pwd -P)/$(basename "$1")
-ln -s "${BASEDIR}pre-push" "${BASEDIR}.git/hooks"
+# Find root of git project
+BASEDIR=$(git rev-parse --show-toplevel)
+# Symbolic link to git hook
+ln -s "${BASEDIR}/tools/pre-push" "${BASEDIR}/.git/hooks"
 echo "Complete"
