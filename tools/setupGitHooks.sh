0b23286493777c599e838f40bb9ae8bd8541f7bf
Rowell Sotto <github@sotto.org>
2016-11-05 20:46:30 -0700

    Harden the git hook setup script

--- tools/setupGitHooks.sh
+++ tools/setupGitHooks.sh
@@ -3,5 +3,5 @@
 # Sets up git hooks to prevent bad checkins from being uploaded to the origin
 echo "Symlinking hooks..."
 BASEDIR=$(cd $(dirname "$1") && pwd -P)/$(basename "$1")
-ln -s "${BASEDIR}pre-push" ../.git/hooks/
+ln -s "${BASEDIR}pre-push" "${BASEDIR}.git/hooks"
 echo "Complete"
